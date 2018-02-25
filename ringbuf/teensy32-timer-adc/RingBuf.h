ca36da27706639721cf1d363ec78005a1f139fdb
isaias.bartelborth@kaufhof.de <isaias.bartelborth@kaufhof.de>
2018-02-25 19:16:58 +0100

    Add ringbuffer test snippets

---
+++ ringbuf/teensy32-timer-adc/RingBuf.h
@@ -0,0 +1,57 @@
+#ifndef __RINGBUF_H__
+#define __RINGBUF_H__
+
+template<typename T, size_t S> class RingBuf
+{
+public:
+  typedef T      value_type;
+
+  RingBuf() { clear(); }
+  ~RingBuf() {}
+    
+  inline size_t size()        const { return size_; }
+  inline size_t capacity()    const { return S; }
+  inline bool isFull()        const { return size_ >= S; }
+  inline bool isEmpty()       const { return size_ <= 0; }
+  inline bool hasElements()   const { return size_ >  0; }
+  inline size_t min(size_t a, size_t b) const { return a < b ? a : b; }
+  inline size_t max(size_t a, size_t b) const { return a > b ? a : b; }
+  
+    
+  value_type& head() { return buffer_[head_]; }
+  value_type& last() { return buffer_[last_]; }
+    
+  void clear() {
+    size_ = 0;
+    head_ = 0;
+    last_ = S - 1;
+  }
+    
+  void push(const value_type& x) {
+    next(last_);
+    if( size_ >= S )
+      next(head_);
+    else
+      size_ = min(size_ + 1, S);
+    last() = x;
+  }
+    
+  value_type& pop() {
+    if( size_ > 0  ) {
+      size_ = max(size_ - 1, 0);
+      next(head_);
+    }
+    return head();
+  }
+
+private:    
+  value_type buffer_[S];    
+  size_t size_;
+  size_t head_;
+  size_t last_;
+
+  inline void next(size_t& x) { x = (x + 1) % S; }
+};
+
+#endif
+
---
+++ ringbuf/teensy32-timer-adc/teensy32-timer-adc.ino
@@ -0,0 +1,74 @@
+//#define USE_DIAGNOSTICS
+//#define USE_BINARY
+
+//           DIAGNOSTICS
+//              no    yes
+// UART bin  20000  17000
+// UART txt   4400   3000
+
+#include "Data.h"
+#include "RingBuf.h"
+#include "ADC.h"
+#include "Timer.h"
+
+const int  sampleRate     = 16000;
+const long serialRate     = 2000000;
+const int  bufferCapacity = 2048;
+RingBuf<MODEL, bufferCapacity> rbuf;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  SerialUSB.begin(serialRate);
+
+  Analog::setup();
+  Timer::setup(sampleRate, measure);
+}
+
+void loop() {
+  static int counter = 0;
+  bool state = (counter++ >> 8) % 2;
+  digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+  push();
+  delayMicroseconds(5);
+}
+
+inline void push() {
+  while (rbuf.hasElements()) {
+    MODEL m = rbuf.pop();
+#ifdef USE_BINARY
+    SerialUSB.write(rbuf.isFull() ? 'f' : 'n');
+    SerialUSB.write(m.bytes, NBYTES);
+#else
+    SerialUSB.print(rbuf.size());
+    for (int i = 0; i < NWORDS; i++) {
+      //if(i != 0) 
+      SerialUSB.print('\t');
+      SerialUSB.print(m.words[i], DEC);
+    }
+#endif
+    SerialUSB.write('\n');
+  }
+}
+
+void measure () {
+  static long counter = 0;
+  MODEL m;
+  m.words[Col::Id] = 4096; //++counter % 4096;
+  m.words[Col::Data0] = Analog::read(A0);
+  m.words[Col::Data1] = Analog::read(A1);
+  m.words[Col::Data2] = Analog::read(A2);
+
+#ifdef USE_DIAGNOSTICS
+  static long t0 = micros();
+  long t1 = micros();
+  long interruptLength = t1 - t0;
+  t0 = t1;
+  m.words[Col::Time] = t1 % 4096;
+  m.words[Col::InterruptLength] = interruptLength;
+  m.words[Col::SampleLength] = micros() - t1;
+#endif
+
+  rbuf.push(m);
+}
+
+
